9b2682d313d658489c7c476e426f9de39739b218
starjiang <82776315@qq.com>
2015-09-17 18:38:32 +0800

    merge 1.2

---
+++ spp/model/CDbMapper.php
@@ -0,0 +1,337 @@
+<?php
+namespace spp\model;
+use spp\model\CConnMgr;
+
+class CDbMapper
+{
+	static $op = ['>'=>'>','>='=>'>=','<'=>'<','<='=>'<=',
+		'='=>'=','like'=>'like','is'=>'is','is not'=>'is not'
+	];
+	private static $instances = array();
+	private $table = null;
+	private $pk = null;
+	private $pdo = null;
+	private $condition = '';
+	private $limit = '';
+	private $order = '';
+	protected function __construct($table,$pk = 'id') {
+		$this->table = $table;
+		$this->pk = $pk;
+		if(isset(\Config::$db)){
+			$this->pdo = CConnMgr::getInstance()->pdo(\Config::$db);
+		}
+	}
+	
+	public static function newInstance()
+	{
+		$caller = get_called_class();
+		return new $caller();
+	}
+	
+	private static function checkOperater($op)
+	{
+		return self::$op[$op] != null;
+	}
+	
+	private static function checkBoolean($bool)
+	{
+		if($bool == 'and' || $bool == 'or'){
+			return true;
+		}
+		return false;
+	}
+	
+	public static function getInstance($table,$pk = 'id')
+	{
+		$key = $table.".".$pk;
+		if(self::$instances[$key] == null)
+		{
+			self::$instances[$key] = new self($table,$pk);
+		}
+		return self::$instances[$key];
+	}
+	
+	function setPdo($pdo)
+	{
+		$this->pdo = $pdo;
+	}
+		
+	public function findByPk($id,$columns = ['*'])
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		$sth = $this->pdo->prepare('select '.$this->getSelectList($columns).' from '.$this->table.' where `'.$this->pk.'` = :id');
+		$sth->execute(array('id'=>$id));
+		$obj = $sth->fetch(\PDO::FETCH_OBJ);
+		return $obj;
+	}
+	
+	public function where($column,$op,$value,$bool='and')
+	{
+		var_dump(self::checkOperater($op));
+		if(!self::checkOperater($op) || !self::checkBoolean($bool))
+		{
+			throw new CModelException("invalid op or bool param");
+		}
+				
+		if($this->condition == '')
+		{
+			$this->condition = " where `$column` $op '".addslashes($value)."'";
+		}
+		else
+		{
+			$this->condition .= " $bool `$column` $op '".addslashes($value)."'";
+		}
+		return $this;
+	}
+	
+	public function limit($count,$offset = 0)
+	{
+		$this->limit = " limit $offset,$count";
+		return $this;
+	}
+	
+	public function orderBy($column,$order = 'desc')
+	{
+		if($this->order == '')
+		{
+			$this->order = " order by `$column` $order";
+		}
+		else
+		{
+			$this->order .= ",`$column` $order"; 
+		}
+		return $this;
+	}
+	
+	public function find($columns = ['*'])
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		
+		$query = 'select '.$this->getSelectList($columns).' from '.$this->table.  $this->condition.  $this->order.  $this->limit;
+		$sth = $this->pdo->prepare($query);
+		$sth->execute();
+		
+		$objs = $sth->fetchAll(\PDO::FETCH_OBJ);
+		$this->condition = '';
+		$this->order = '';
+		$this->limit = '';
+		
+		return $objs;
+	}
+	
+	public function count()
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		
+		$query = 'select count(*) as total from '.$this->table.  $this->condition.  $this->order.  $this->limit;
+
+		$sth = $this->pdo->prepare($query);
+		$sth->execute();
+		
+		$obj = $sth->fetch(\PDO::FETCH_OBJ);
+		$this->condition = '';
+		$this->order = '';
+		$this->limit = '';
+		return $obj->total;
+	}
+	
+	public function distinct($column)
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		
+		$query = 'select distinct('.$column.') from '.$this->table.  $this->condition;
+
+		$sth = $this->pdo->prepare($query);
+		$sth->execute();
+		
+		$objs = $sth->fetchAll(\PDO::FETCH_OBJ);
+		$this->condition = '';
+		$this->order = '';
+		$this->limit = '';
+		
+		$list = array();
+		foreach ($objs as $obj)
+		{
+			$list[] = $obj->$column;
+		}
+		
+		return $list;
+	}
+	
+	
+	public function whereIn($column,$values,$bool = 'and')
+	{
+		if(!is_array($values) && count($values))
+		{
+			throw new CModelException("param values is not a array");
+		}
+		if($this->condition == '')
+		{
+			$this->condition = " where `$column` in ('".  implode("','", $values)."')";
+		}
+		else
+		{
+			$this->condition .= " $bool `$column` in ('".  implode("','", $values)."')";
+		}
+		
+		return $this;
+	}
+	
+	public function whereNotIn($column,$values,$bool = 'and')
+	{
+		if(!is_array($values) && count($values))
+		{
+			throw new CModelException("param values is not a array");
+		}
+		if($this->condition == '')
+		{
+			$this->condition = " where `$column` not in (".  implode(",", $values).")";
+		}
+		else
+		{
+			$this->condition .= " $bool `$column` not in (".  implode(",", $values).")";
+		}
+		return $this;
+	}
+	
+	public function insert($obj)
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		$query = 'insert into '.$this->table. $this->getInsertList($obj);
+		$sth = $this->pdo->prepare($query);
+		$sth->execute();
+		return $this->pdo->lastInsertId();
+	}
+	
+	public function update($obj)
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		$query = 'update '.$this->table.' set '. $this->getUpdateList($obj).$this->condition;
+		$sth = $this->pdo->prepare($query);
+		$sth->execute();
+		$this->condition = '';
+		$this->order = '';
+		$this->limit = '';
+	}
+	
+	public function updateByPk($obj)
+	{
+		$pk = $this->pk;
+		if($obj->$pk == 0 || $obj->$pk == '')
+		{
+			throw new CModelException("primary key is empty");			
+		}
+		$this->where($pk,'=',$obj->$pk)->update($obj);
+	}
+	
+	public function save($obj)
+	{
+		$pk = $this->pk;
+		
+		if($obj->$pk == 0 || $obj->$pk == '')
+		{
+			throw new CModelException("primary key is empty");			
+		}
+		$preObj = $this->findByPk($obj->$pk);		
+		if($preObj == false)
+		{
+			$this->insert($obj);
+		}
+		else
+		{
+			$this->where($pk,'=',$obj->$pk)->update($obj);
+		}
+	}
+	public function deleteByPk($id)
+	{
+		$this->where($this->pk, '=', $id)->delete();
+	}
+	public function delete()
+	{
+		if($this->pdo == null){
+			throw new CModelException("pdo is null");
+		}
+		$query = 'delete from '.$this->table.$this->condition;
+		$sth = $this->pdo->prepare($query);
+		$sth->execute();
+		$this->condition = '';
+		$this->order = '';
+		$this->limit = '';
+	}
+	private function getSelectList($columns)
+	{
+		$list = '';
+		foreach($columns as $column)
+		{
+			if($list == '')
+			{
+				($column != '*') ? ($list = "`".$column."`" ): ($list = $column);
+			}
+			else 
+			{
+				($column != '*') ? ($list .= ",`".$column."`" ): ($list .=','.column);
+			}
+		}
+		return $list;
+	}
+	
+	private function getInsertList($obj)
+	{
+		$keys = '';
+		$values = '';
+		foreach ($obj as $key=>$value)
+		{
+			if($keys == '')
+			{
+				$keys = "`$key`";
+			}
+			else
+			{
+				$keys .=",`$key`";
+			}
+			
+			if($values == '')
+			{
+				$values = "'".addslashes($value)."'";
+			}
+			else
+			{
+				$values .=",'".addslashes($value)."'";
+			}
+		}
+	
+		$list = ' ('.$keys.') values ('.$values.')';
+		
+		return $list;
+	}
+	private function getUpdateList($obj)
+	{
+		$list = '';
+		foreach ($obj as $key=>$value)
+		{
+			if($list == '')
+			{
+				$list = "`$key` = '".addslashes($value)."'";
+			}
+			else
+			{
+				$list .= ",`$key` = '".addslashes($value)."'";
+			}
+		}
+		return $list;
+	}
+	
+}
+
---
+++ spp/model/CMongoMapper.php
@@ -0,0 +1,292 @@
+<?php
+namespace spp\model;
+use spp\model\CConnMgr;
+
+class CMongoMapper
+{
+	static $op = ['>'=>'$gt','>='=>'$gte','<'=>'$lt','<='=>'$lte',
+		'='=>'=','in'=>'$in','not in'=>'$nin'
+	];
+	private static $instances = array();
+	private $collection = null;
+	private $mongo = null;
+	private $condition = [];
+	private $count = 0;
+	private $offset = 0;
+	private $order = [];
+	private $pk = '_id';
+	protected function __construct($collection) {
+		$this->collection = $collection;
+		if(isset(\Config::$mongo)){
+			$this->mongo = CConnMgr::getInstance()->mongo(\Config::$mongo);
+		}
+	}
+	
+	public static function newInstance()
+	{
+		$caller = get_called_class();
+		return new $caller();
+	}
+	
+	private static function checkOperater($op)
+	{
+		return self::$op[$op] != null;
+	}
+	
+	private static function getOperater($op)
+	{
+		return self::$op[$op];
+	}
+	private static function checkBoolean($bool)
+	{
+		if($bool == 'and' || $bool == 'or'){
+			return true;
+		}
+		return false;
+	}
+	
+	public static function getInstance($collection)
+	{
+		$key = $collection;
+		if(self::$instances[$key] == null)
+		{
+			self::$instances[$key] = new self($collection);
+		}
+		return self::$instances[$key];
+	}
+	
+	function setMongo($mongo)
+	{
+		$this->mongo = $mongo;
+		
+	}
+		
+	public function findByPk($id,$columns = [])
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		
+		$collection = $this->collection;
+		$obj = $this->mongo->$collection->findOne(array('_id' => $id),$columns);
+		return $obj;
+	}
+	
+	public function where($column,$op,$value,$bool='and')
+	{
+		if(!self::checkOperater($op) || !self::checkBoolean($bool))
+		{
+			throw new CModelException("invalid op or bool param");
+		}
+		
+		$op = self::getOperater($op);
+		
+		if(count($this->condition) == 0)
+		{
+			if($op == '=')	{
+				$this->condition = [$column=>$value];
+			}
+			else {
+				$this->condition = [$column => [$op=>$value]];
+			}
+		}
+		else
+		{
+			if($bool == 'and')
+			{
+				if($op == '='){
+					$this->condition[$column] = $value;
+				}
+				else
+				{
+					$condition[$column] = [$op=>$value];
+				}
+			}
+			else
+			{
+				$condition = $this->condition;
+				$this->condition = [];
+				$this->condition['$or'][] = [$column=>$value];
+				$this->condition['$or'][] = $condition;
+			}
+		}
+		return $this;
+	}
+	
+	public function limit($count,$offset = 0)
+	{
+		$this->count = $count;
+		$this->offset = $offset;
+		return $this;
+	}
+	
+	public function orderBy($column,$order = 'desc')
+	{
+		if(count($this->order) == 0)
+		{
+			if($order == 'desc')
+				$this->order = [$column=>0];
+			else 
+				$this->order = [$column=>1];
+		}
+		else
+		{
+			if($order == 'desc')
+				$this->order[$column] = 0;
+			else
+				$this->order[$column] = 1;
+		}
+		return $this;
+	}
+	
+	public function find($columns = [])
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		$collection = $this->collection;
+	
+		$rows = false;
+		
+		if($this->count!=0 && $this->offset != 0)
+		{
+			$rows = $this->mongo->$collection->find($this->condition,$columns)->sort($this->order)->limit($this->count)->skip($this->offset);
+		}
+		else if($this->count !=0 && $this->offset == 0)
+		{
+			$rows = $this->mongo->$collection->find($this->condition,$columns)->sort($this->order)->limit($this->count);
+		}
+		else if($this->count ==0 && $this->offset != 0)
+		{
+			$rows = $this->mongo->$collection->find($this->condition,$columns)->sort($this->order)->skip($this->offset);
+		}
+		else
+		{
+			$rows = $this->mongo->$collection->find($this->condition,$columns)->sort($this->order);
+		}
+		
+		$this->condition = [];
+		$this->order = [];
+		$this->count = 0;
+		$this->offset = 0;
+		
+		return $rows;
+	}
+	
+	public function count()
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		
+		$collection = $this->collection;
+		
+		return $this->mongo->$collection->find($this->condition,$columns)->count();
+	}
+	
+	public function distinct($column)
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		
+		$collection = $this->collection;
+		return $this->mongo->$collection->distinct($column);
+	}
+	
+	
+	public function whereIn($column,$values,$bool = 'and')
+	{
+		if(!is_array($values) && count($values))
+		{
+			throw new CModelException("param values is not a array");
+		}
+		return $this->where($column, 'in', $value,$bool);
+	}
+	
+	public function whereNotIn($column,$values,$bool = 'and')
+	{
+		if(!is_array($values) && count($values))
+		{
+			throw new CModelException("param values is not a array");
+		}
+		return $this->where($column, 'not in', $value,$bool);
+	}
+	
+	public function insert($obj)
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		
+		$collection = $this->collection;
+		$this->mongo->$collection->insert($obj);
+		return $obj->_id;
+	}
+	
+	public function update($obj)
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		
+		$collection = $this->collection; 
+		
+		$this->mongo->$collection->update($this->condition,$obj);
+		
+		$this->condition = [];
+		$this->order = [];
+		$this->count = 0;
+		$this->offset = 0;
+	}
+	
+	public function updateByPk($obj)
+	{
+		$pk = $this->pk;
+		if($obj->$pk == 0 || $obj->$pk == '')
+		{
+			throw new CModelException("primary key is empty");			
+		}
+		$this->where($pk,'=',$obj->$pk)->update($obj);
+	}
+	
+	public function save($obj)
+	{
+		$pk = $this->pk;
+		
+		if($obj->$pk == 0 || $obj->$pk == '')
+		{
+			throw new CModelException("primary key is empty");			
+		}
+		$preObj = $this->findByPk($obj->$pk);		
+		if($preObj == false)
+		{
+			$this->insert($obj);
+		}
+		else
+		{
+			$this->where($pk,'=',$obj->$pk)->update($obj);
+		}
+	}
+	
+	public function deleteByPk($id)
+	{
+		$this->where($this->pk, '=', $id)->delete();
+	}
+	
+	public function delete()
+	{
+		if($this->mongo == null){
+			throw new CModelException("mongo is null");
+		}
+		
+		$collection = $this->collection; 
+		$this->mongo->$collection->remove($this->condition);
+		$this->condition = [];
+		$this->order = [];
+		$this->count = 0;
+		$this->offset = 0;
+	}
+	
+}
+
